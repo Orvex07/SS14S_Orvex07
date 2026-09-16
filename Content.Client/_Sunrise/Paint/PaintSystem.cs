f935929e61847bbc369af51cde622a03ff899f75
Orvex07 <77834935+Orvex07@users.noreply.github.com>
2026-09-16 10:26:07 +0200

    Update Content.Client/_Sunrise/Paint/PaintSystem.cs

--- Content.Client/_Sunrise/Paint/PaintSystem.cs
+++ Content.Client/_Sunrise/Paint/PaintSystem.cs
@@ -4,7 +4,7 @@ namespace Content.Client._Sunrise.Paint;
 
 public sealed class PaintSystem : SharedPaintSystem
 {
-    [Dependency] private readonly SharedUserInterfaceSystem _ui = default!;
+    [Dependency] private SharedUserInterfaceSystem _ui = default!;
 
     public override void Initialize()
     {
